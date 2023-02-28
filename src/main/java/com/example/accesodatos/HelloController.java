d73a3603ec3672461dfb8e9642baae19afa12477
Alfreditto <alfredo_lajara@outlook.es>
2023-02-28 20:04:47 +0100

    Obtiene datos

--- src/main/java/com/example/accesodatos/HelloController.java
+++ src/main/java/com/example/accesodatos/HelloController.java
@@ -3,15 +3,19 @@ package com.example.accesodatos;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.xmldb.api.DatabaseManager;
-import org.xmldb.api.base.Collection;
-import org.xmldb.api.base.Database;
-import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.XPathQueryService;
 
 
 public class HelloController {
 
-
+    public TextField txtNumDep;
+    public TextField txtNombre;
+    public TextField txtDep;
+    public TextField txtLocalidad;
+    Collection col;
     @FXML
     private Label welcomeText;
 
@@ -29,11 +33,12 @@ public class HelloController {
             Database database = (Database) cl.newInstance();
             DatabaseManager.registerDatabase(database);
 
-            String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/Pruebas";
+            String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/Prueba";
             String usu = "admin";
             String usuPwd = "pw";
-            Collection col = DatabaseManager.getCollection(URI, usu, usuPwd);
+            col = DatabaseManager.getCollection(URI, usu, usuPwd);
             System.out.println(col.getName());
+//            XPathQueryService service = (XPathQueryService) col.getServices()
         } catch (ClassNotFoundException | XMLDBException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
@@ -51,7 +56,32 @@ public class HelloController {
     public void btnLimpiarClick(ActionEvent actionEvent) {
     }
 
-    public void btnBuscarClick(ActionEvent actionEvent) throws ClassNotFoundException, InstantiationException, IllegalAccessException, XMLDBException {
-
+    public void btnBuscarClick(ActionEvent actionEvent) {
+        XPathQueryService service;
+        try {
+            service = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+            String query = "/departamentos/DEP_ROW[DEPT_NO=" + txtNumDep.getText() + "]/DNOMBRE/text()";
+            String query2 = "/departamentos/DEP_ROW[DEPT_NO=" + txtNumDep.getText() + "]/LOC/text()";
+            ResourceSet resultNombre = service.query(query);
+            ResourceSet resultLoc = service.query(query2);
+            System.out.println(resultNombre.getSize());
+            System.out.println(resultLoc.getSize());
+            ResourceIterator i = resultNombre.getIterator();
+            ResourceIterator j = resultLoc.getIterator();
+            if (!i.hasMoreResources() && !j.hasMoreResources()) {
+                System.out.println("Consulta nulla");
+            } else {
+                while (i.hasMoreResources() && j.hasMoreResources()) {
+                    Resource r = i.nextResource();
+                    Resource rs = j.nextResource();
+                    txtNombre.setText((String) r.getContent());
+                    txtLocalidad.setText((String) rs.getContent());
+                    System.out.println((String) r.getContent());
+                    System.out.println((String) rs.getContent());
+                }
+            }
+        } catch (XMLDBException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
